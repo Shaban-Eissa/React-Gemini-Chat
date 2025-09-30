2b78070543faec06f92eb832a14945a533c3ca15
Shaban-Eissa <shabaneissa767@gmail.com>
2025-09-30 19:11:19 +0300

    🎉 Initial Commit: add initial files for the repo

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Chat from "./components/Chat";
+
+function App() {
+  return (
+    <div className="min-h-screen flex bg-gray-50 dark:bg-gray-900">
+      <main className="flex-1 min-w-0">
+        <Chat />
+      </main>
+    </div>
+  );
+}
+
+export default App;
